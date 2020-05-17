d8d59d7a553c95d725dfe162d5a71d25e48e7b89
Tommaso Visconti <tommaso.visconti@gmail.com>
2020-05-17 22:05:30 +0200

    move balls around

--- breakout.go
+++ breakout.go
@@ -13,22 +13,30 @@ const (
 )
 
 type player struct {
-	w     float64
-	h     float64
+	w, h  float64
 	speed float64
-	x     float64
-	y     float64
+	x, y  float64
 	color color.RGBA
 	image *ebiten.Image
 }
 
+type ball struct {
+	radius    float64
+	x, y      float64
+	speed     float64
+	direction [2]float64
+	color     color.RGBA
+	image     *ebiten.Image
+}
+
 type Game struct {
-	p *player
+	p     *player
+	balls []*ball
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
+	s_w, s_h := screen.Size()
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		s_w, _ := screen.Size()
 		if g.p.x+g.p.w+g.p.speed <= float64(s_w) {
 			g.p.x += g.p.speed
 		}
@@ -38,10 +46,34 @@ func (g *Game) Update(screen *ebiten.Image) error {
 			g.p.x = 0
 		}
 	}
+	for _, b := range g.balls {
+		new_x := b.x + b.direction[0]*b.speed
+
+		if new_x < 0 || new_x+2*b.radius > float64(s_w) {
+			b.direction[0] = -b.direction[0]
+		}
+		b.x += b.direction[0] * b.speed
+
+		new_y := b.y + b.direction[1]*b.speed
+		if new_y < 0 {
+			b.direction[1] = -b.direction[1]
+		} else if new_y+2*b.radius > float64(s_h) {
+			// TODO: this is the lose condition
+
+			// inverting the direction for debugging purposes
+			b.direction[1] = -b.direction[1]
+		}
+		b.y += b.direction[1] * b.speed
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	g.drawPlayer(screen)
+	g.drawBalls(screen)
+}
+
+func (g *Game) drawPlayer(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
 
 	red := float64(g.p.color.R) / 0xff
@@ -55,6 +87,28 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		log.Fatal(err)
 	}
 }
+
+func (g *Game) drawBalls(screen *ebiten.Image) {
+	for _, b := range g.balls {
+		g.drawBall(screen, b)
+	}
+}
+
+func (g *Game) drawBall(screen *ebiten.Image, b *ball) {
+	op := &ebiten.DrawImageOptions{}
+
+	red := float64(b.color.R) / 0xff
+	green := float64(b.color.G) / 0xff
+	blue := float64(b.color.B) / 0xff
+	op.ColorM.Translate(red, green, blue, 1)
+
+	op.GeoM.Translate(b.x, b.y)
+
+	if err := screen.DrawImage(b.image, op); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return W_WIDTH, W_HEIGHT
 }
@@ -76,6 +130,19 @@ func (g *Game) Init() {
 		color: color.RGBA{0xff, 0x00, 0x00, 0xff},
 		image: i,
 	}
+	b_i, err := ebiten.NewImage(20, 20, ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.balls = append(g.balls, &ball{
+		radius:    10,
+		x:         float64(W_WIDTH/2 - 10*2/2),
+		y:         10,
+		speed:     10,
+		direction: [2]float64{0, 1},
+		color:     color.RGBA{0xff, 0x00, 0xff, 0xff},
+		image:     b_i,
+	})
 }
 
 func Start() {
